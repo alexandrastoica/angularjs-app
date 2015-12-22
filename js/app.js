1b892be8145663d4d99cef8290e239fb2e9814a8
alexandrastoica <alexandra.stoica95@yahoo.com>
2015-12-22 15:08:54 +0200

    Fix bug in app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('photoApp', ['ngRoute', 'ngSanitize' 'photoControllers']);
+var app = angular.module('photoApp', ['ngRoute', 'ngSanitize', 'photoControllers']);
 
 app.config(['$routeProvider',
   function($routeProvider) {

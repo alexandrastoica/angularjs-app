53a4d0a6b60779e189e6cf9e5c938c3a78e86fd9
alexandrastoica <alexandra.stoica95@yahoo.com>
2016-02-25 23:52:17 +0000

    improve search

--- js/controllers.js
+++ js/controllers.js
@@ -1,38 +1,45 @@
 var photoControllers = angular.module('photoControllers', []);
 
-photoControllers.controller('MainCtrl', function ($scope, Photo){
-    $scope.dataLoaded = false;
-    $scope.getData = function(tag){
-        Photo.load({tags: tag}, function(data){
+photoControllers.controller('MainCtrl', function($scope, Photo) {
+
+    Photo.load({ tags: 'world' }, function(data) {
+        $scope.photos = data.items;
+
+        var pagesShown = 1;
+        var pageSize = 5;
+
+        $scope.photoLimit = function() {
+            return pageSize * pagesShown;
+        }
+        $scope.hasMoreItemsToShow = function() {
+            return pagesShown < ($scope.photos.length / pageSize);
+        }
+        $scope.showMoreItems = function() {
+            pagesShown = pagesShown + 1;
+        }
+
+    });
+
+    $scope.getData = function(tag) {
+        $scope.photos = [];
+
+        Photo.load({ tags: tag }, function(data) {
             $scope.photos = data.items;
             console.log($scope.photos);
-            $scope.dataLoaded = true;
         });
     }
+
 });
 
-photoControllers.controller('GalleryController', ['$scope', function ($scope){
-
-    var pagesShown = 1;
-    var pageSize = 5;
-
-    $scope.photoLimit = function() {
-        return pageSize * pagesShown;
-    };
-    $scope.hasMoreItemsToShow = function() {
-        return pagesShown < ($scope.photos.length / pageSize);
-    };
-    $scope.showMoreItems = function() {
-        pagesShown = pagesShown + 1;         
-    };
-    
-}]);
+photoControllers.controller('GalleryController', function($scope, Photo) {
 
 
-photoControllers.controller('PhotoDetailCtrl', ['$scope', '$routeParams', function ($scope, $routeParams){
-	
+
+});
+
+
+photoControllers.controller('PhotoDetailCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+
     $scope.photo_id = $routeParams.photoID;
 
 }]);
-
-
